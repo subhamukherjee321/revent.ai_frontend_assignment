9e7fb3740294341abd8ebfdd21fa1ef59edeba83
subhamukherjee321 <subhamukherjee321@gmail.com>
2023-10-07 21:16:31 +0530

    Responsive part is allmost done

--- frontend/src/Components/Pagination.jsx
+++ frontend/src/Components/Pagination.jsx
@@ -1,28 +1,36 @@
 import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
+import { useProduct } from "../Store/Home";
 
-const Pagination = ({ page, changePage }) => {
+const Pagination = () => {
+  const { page, setPage } = useProduct((state) => state);
+
   return (
     <Flex justify={"center"} align={"center"} my={6} gap={4}>
       <Button
         bg={"gray.900"}
         color={"white"}
-        onClick={() => changePage(-1)}
+        onClick={() => setPage(page - 1)}
         isDisabled={page === 1}
         _hover={{ bg: "gray.600" }}
-        _active={{bg: "gray.800"}}
+        _active={{ bg: "gray.800" }}
       >
         Previous
       </Button>
-      <Button bg={"gray.900"} _hover={{ bg: "gray.900" }} color={"white"} isDisabled>
+      <Button
+        bg={"gray.900"}
+        _hover={{ bg: "gray.900" }}
+        color={"white"}
+        isDisabled
+      >
         {page}
       </Button>
       <Button
         bg={"gray.900"}
         color={"white"}
         _hover={{ bg: "gray.600" }}
-        _active={{bg: "gray.800"}}
-        onClick={() => changePage(1)}
+        _active={{ bg: "gray.800" }}
+        onClick={() => setPage(page + 1)}
       >
         Next
       </Button>
--- frontend/src/Components/SearchBar.jsx
+++ frontend/src/Components/SearchBar.jsx
@@ -1,50 +1,24 @@
 import React, { useState } from "react";
 import PrimaryBtn from "./PrimaryBtn";
 
-const SearchBar = () => {
+function SearchBar() {
   const [inputValue, setInputValue] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(inputValue);
   };
 
   return (
-    <form
-      onSubmit={submitHandler}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "1rem",
-      }}
-    >
+    <form onSubmit={submitHandler} className="responsive-form">
       <input
         type="text"
         placeholder="Search any product"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        style={{
-          padding: "0.56rem 4rem",
-          border: "none",
-          outline: "none",
-          borderBottom: "1px solid black",
-          borderRadius: "0.2rem",
-          fontWeight: "bold"
-        }}
+        className="input-field"
       />
-      <PrimaryBtn text={"Search"} />
     </form>
   );
-};
+}
 
 export default SearchBar;
-
-
-/*
-Before Start Do create new branch
-Is Responsive part starting
-Then Responsive Product Page Branch
-For Functionality Give name of functionality
-For Single Product Page give name single product page
-*/
